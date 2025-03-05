4b1b7b95bdfa3134073ade5154ca1a687642b5c5
michalwachowicz <wachowiczmichal1@gmail.com>
2025-03-05 11:58:13 +0100

    Create a web server functionality

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 require("dotenv").config();
 
 const http = require("http");
+const path = require("path");
+const fs = require("fs/promises");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer(async (req, res) => {
+  const filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  const ext = path.extname(filePath);
+
+  let contentType = "text/html";
+
+  switch (ext) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+  }
+
+  try {
+    const data = await fs.readFile(filePath);
+
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      const data = await fs.readFile(
+        path.join(__dirname, "public", "404.html")
+      );
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    } else {
+      console.error(err);
+      res.writeHead(500);
+      res.end(`Server error: ${err.code}`);
+    }
+  }
+});
 
 const PORT = process.env.PORT || 8080;
 
